62de21e3aa4c84e57a2ed7b9c92cc28171555983
Vipin Jain <vipin.jain@prevoir.mu>
2022-08-30 15:50:47 +0530

    latest

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  $schema: 'http://json.schemastore.org/prettierrc',
+  printWidth: 150,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSameLine: true
+}
